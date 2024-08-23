e43ea865c8e7253e0c6f77c166f90af9cd835fe4
PBitMinecraft <109835351+PBitMinecraft@users.noreply.github.com>
2024-08-23 12:39:34 -0400

    Publish

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows.Input;
+using System.Windows;
+
+namespace WebView_Browser
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeBrowser();
+        }
+
+        private async void InitializeBrowser()
+        {
+            await Browser.EnsureCoreWebView2Async(null);
+            Browser.Source = new Uri("https://pbitminecraft.github.io/");
+        }
+
+        private void AddressBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                NavigateToAddress();
+            }
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToAddress();
+        }
+
+        private void NavigateToAddress()
+        {
+            var address = AddressBar.Text;
+            if (!address.StartsWith("http"))
+            {
+                address = "https://" + address;
+            }
+
+            Browser.Source = new Uri(address);
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Browser.CanGoBack)
+            {
+                Browser.GoBack();
+            }
+        }
+
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Browser.CanGoForward)
+            {
+                Browser.GoForward();
+            }
+        }
+
+        private void ReloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Browser.Reload();
+        }
+    }
+}
